b14ae7d5ac20ea01f71c22f0be0f5e0944483f61
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-11-22 16:10:26 +0800

    Insert published property to the Book constructor

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 const myLibrary = [];
 
 class Book {
-  constructor(title, author) {
+  constructor(title, author, published) {
     this.title = title;
     this.author = author;
+    this.published = published;
   }
 }
 
 // Filler Data
 
-myLibrary.push(new Book("A Discipline of Programming", "Edsger W. Dijkstra"));
-myLibrary.push(new Book("Das Kapital Volume I", "Karl Marx"));
+myLibrary.push(new Book("A Discipline of Programming", "Edsger W. Dijkstra", "1976"));
+myLibrary.push(new Book("Das Kapital, Volume I", "Karl Marx", "1867"));
 
 console.dir (myLibrary);
