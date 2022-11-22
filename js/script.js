1990867bd948d961e3501d54fff51b36515557e3
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-11-22 16:28:01 +0800

    Create a DOM func for newBook

--- js/script.js
+++ js/script.js
@@ -14,3 +14,36 @@ myLibrary.push(new Book("A Discipline of Programming", "Edsger W. Dijkstra", "19
 myLibrary.push(new Book("Das Kapital, Volume I", "Karl Marx", "1867"));
 
 console.dir (myLibrary);
+
+// DOM Stuff
+
+const bookList = document.querySelector(".book-list");
+
+const newBook = function(title, author, published) {
+  const book = document.createElement("div");
+  book.classList.add("book");
+
+  const bookTitleH1 = document.createElement("h1");
+  bookTitleH1.classList.add("book-title");
+  bookTitleH1.textContent = title;
+
+  const bookAttrib = document.createElement("ul");
+  
+  const bookAuthor = document.createElement("li")
+  bookAuthor.classList.add("book-author");
+  bookAuthor.textContent = `Author: ${author}`;
+
+  const bookPublished = document.createElement("li")
+  bookPublished.classList.add("book-published");
+  bookPublished.textContent = `Published: ${published}`;
+  
+  bookAttrib.appendChild(bookAuthor);
+  bookAttrib.appendChild(bookPublished);
+
+  book.appendChild(bookTitleH1);
+  book.appendChild(bookAttrib);
+
+  return book;
+}
+
+
