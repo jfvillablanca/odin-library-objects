55baf0c58c2b2caaaf97eda735a6e35a3fd0fa52
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-11-23 15:12:32 +0800

    Consolidate createNewBook function as a static method to the Book class

--- js/script.js
+++ js/script.js
@@ -8,41 +8,43 @@ class Book {
     this.haveRead = haveRead;
   }
 
+  static createNewBook(title, author, published, haveRead) {
+    // return newBook(this.title, this.author, this.published, this.haveRead);
+    const book = document.createElement("div");
+    book.classList.add("book");
+
+    const bookTitleH1 = document.createElement("h1");
+    bookTitleH1.classList.add("book-title");
+    bookTitleH1.textContent = title;
+
+    const bookAttrib = document.createElement("ul");
+
+    const bookAuthor = document.createElement("li");
+    bookAuthor.classList.add("book-author");
+    bookAuthor.textContent = `Author: ${author}`;
+
+    const bookPublished = document.createElement("li");
+    bookPublished.classList.add("book-published");
+    bookPublished.textContent = `Published: ${published}`;
+
+    const bookRead = document.createElement("li");
+    bookRead.classList.add("book-have-read");
+    haveRead
+      ? (bookRead.textContent = "I have read this book already.")
+      : (bookRead.textContent = "I have NOT read this book yet.");
+
+    bookAttrib.appendChild(bookAuthor);
+    bookAttrib.appendChild(bookPublished);
+    bookAttrib.appendChild(bookRead);
+
+    book.appendChild(bookTitleH1);
+    book.appendChild(bookAttrib);
+
+    return book;
+  }
+}
+
 // Filler Data
-
-myLibrary.push(new Book("Republic", "Plato", "375 BC"));
-myLibrary.push(new Book("A Discipline of Programming", "Edsger W. Dijkstra", "1976"));
-myLibrary.push(new Book("Das Kapital, Volume I", "Karl Marx", "1867"));
-
-console.dir (myLibrary);
-
-// DOM Stuff
-
-const bookList = document.querySelector(".book-list");
-
-const newBook = function(title, author, published) {
-  const book = document.createElement("div");
-  book.classList.add("book");
-
-  const bookTitleH1 = document.createElement("h1");
-  bookTitleH1.classList.add("book-title");
-  bookTitleH1.textContent = title;
-
-  const bookAttrib = document.createElement("ul");
-  
-  const bookAuthor = document.createElement("li")
-  bookAuthor.classList.add("book-author");
-  bookAuthor.textContent = `Author: ${author}`;
-
-  const bookPublished = document.createElement("li")
-  bookPublished.classList.add("book-published");
-  bookPublished.textContent = `Published: ${published}`;
-  
-  bookAttrib.appendChild(bookAuthor);
-  bookAttrib.appendChild(bookPublished);
-
-  book.appendChild(bookTitleH1);
-  book.appendChild(bookAttrib);
 
 myLibrary.push(new Book("Republic", "Plato", "375 BC", false));
 myLibrary.push(new Book("A Discipline of Programming", "Edsger W. Dijkstra", "1976", true));
